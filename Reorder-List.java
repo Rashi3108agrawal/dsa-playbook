13dccc39fd39a358052e41639bfd9e7d72d9efa5
Rashika Agrawal <rashi3108agrawal@gmail.com>
2026-05-21 17:16:09 +0530

    [143] [Time Beats: 88.80%] [Memory Beats: 93.09%] - LeetPush

--- Reorder-List.java
+++ Reorder-List.java
@@ -9,35 +9,39 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode reverse(ListNode head) {
-13        if(head == null || head.next == null) return head;
-14        ListNode prev = null;
-15        ListNode curr = head;
-16        while(curr!= null){
-17            ListNode nxt = curr.next;
-18            curr.next = prev;
-19            prev = curr;
-20            curr= nxt;
-21        }
-22        return prev;
-23    }
-24    public void reorderList(ListNode head) {
-25        ListNode slow = head;
-26        ListNode fast = head;
-27        while(fast!= null && fast.next!= null){
-28            slow = slow.next;
-29            fast = fast.next.next;
-30        }
-31        ListNode st = head;
-32        ListNode mid = reverse(slow.next);
-33        slow.next = null;
-34        while(mid!=null){
-35            ListNode next1 = st.next;
-36            ListNode next2 = mid.next;
-37            st.next = mid;
-38            mid.next = next1;
-39            st= next1;
-40            mid = next2;
-41        }
-42    }
-43}
+12
+13    public ListNode reverse(ListNode head){
+14        if(head ==null || head.next == null) return head;
+15        ListNode prev= null;
+16        ListNode curr= head;
+17
+18        while(curr!= null){
+19            ListNode next = curr.next;
+20            curr.next = prev;
+21            prev= curr;
+22            curr= next;
+23        }
+24        return prev;
+25    }
+26    public void reorderList(ListNode head) {
+27        if(head == null || head.next ==null) return;
+28        ListNode slow= head;
+29        ListNode fast = head;
+30        while(fast!=null && fast.next!=null){
+31            slow = slow.next;
+32            fast = fast.next.next;
+33        }
+34        ListNode mid = reverse(slow.next);
+35        slow.next = null;
+36        ListNode curr = head;
+37        while(curr!=null && mid!=null){
+38            ListNode next = curr.next;
+39            ListNode midNext = mid.next;
+40            curr.next= mid;
+41            mid.next = next;
+42            curr = next;
+43            mid = midNext;
+44        }
+45        return;
+46    }
+47}
