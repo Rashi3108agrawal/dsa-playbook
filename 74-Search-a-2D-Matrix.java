fca252d912555e67b9b7c1a1bb8e22e31a5462bd
Rashika Agrawal <rashi3108agrawal@gmail.com>
2026-07-18 15:44:18 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 95.83%] - LeetPush

---
+++ 74-Search-a-2D-Matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       int n = matrix.length;
+       int m = matrix[0].length;
+       if(n==1 && m==1){
+            return matrix[0][0] == target;
+       }
+       int low =0;
+       int high = n*m-1;
+       while(low<=high){
+            int mid = low + (high-low)/2;
+            int i = mid/m;
+            int j = mid%m;
+            if(matrix[i][j]== target){
+                return true;
+            }else if(matrix[i][j]> target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+       } 
+       return false;
+    }
+}
