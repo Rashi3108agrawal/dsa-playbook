73f1074f3033811afc5c7bf347285deead54deb9
Rashika Agrawal <rashi3108agrawal@gmail.com>
2026-07-24 22:19:50 +0530

    [455] [Time Beats: 99.02%] [Memory Beats: 74.33%] - LeetPush

---
+++ 455-Assign-Cookies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int res =0;
+        int n  = g.length;
+        int m = s.length;
+        int i=0;
+        int j=0;
+
+        while(i<n && j<m){
+            if(s[j]>=g[i]){
+                res++;
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return res;
+    }
+}
