92c5bc2093f40197e938382d3cc919f36599e775
Rashika Agrawal <rashi3108agrawal@gmail.com>
2026-07-24 22:35:21 +0530

    [322] [Time Beats: 42.20%] [Memory Beats: 50.80%] - LeetPush

--- 322-Coin-Change.java
+++ 322-Coin-Change.java
@@ -8,10 +8,9 @@ class Solution {
         for(int amt =1;amt<= amount;amt++){
             for(int i=0;i<n;i++){
                 int coin = coins[i];
-                int pick = Integer.MAX_VALUE;
-                if(amt>=coin && dp[amt - coin] != Integer.MAX_VALUE) pick = 1+ dp[amt-coin];
-                int notpick = dp[amt];
-                dp[amt] = Math.min(pick, notpick);
+                if (amt >= coin && dp[amt - coin] != Integer.MAX_VALUE) {
+                    dp[amt] = Math.min(dp[amt], dp[amt - coin] + 1);
+                }
             }
         }
         return dp[amount]== Integer.MAX_VALUE? -1: dp[amount];
